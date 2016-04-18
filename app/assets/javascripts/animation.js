922f09afbd29c831736f982fc732af926cc74687
irenemoreno <irene.morenocano@gmail.com>
2016-04-18 10:35:49 +0200

    images

---
+++ app/assets/javascripts/animation.js
@@ -0,0 +1,16 @@
+// var imagenes = ['/animacion/gif2.png','/animacion/gif3.png','/animacion/gif4.png'];
+//var x = 0;
+//var reproducir = function() {
+//	if (x > imagenes.length)
+//	{
+//	x = 0;
+//	}
+//	$("#imagenjq").attr('src', imagenes[x]);
+//	x++;
+//
+//	var timer = setTimeout('reproducir()', 800);
+//	}
+//
+//	$(function(){
+//	reproducir();
+//});
